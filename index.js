b4bd857ff2954fe4e1fadf66a25bc1f8c3dca7de
ombalaji03 <ombalaji2003@gmail.com>
2024-03-28 14:59:18 +0530

    code refactor

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
+const fs = require("fs")
 const readline = require("node:readline/promises");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 async function main() {
-  const sampleNumber = await rl.question("Enter sample number: ");
-  const color = await rl.question("Enter color: ");
-  const form = await rl.question("Enter form: ");
-  const lusture = await rl.question("Enter lusture: ");
-  const streak = await rl.question("Enter streak: ");
-  const hardness = await rl.question("Enter hardness: ");
-  const cleavage = await rl.question("Enter cleavage: ");
-  const fracture = await rl.question("Enter fracture: ");
-  const specificGravity = await rl.question("Enter specific gravity: ");
-  const chemicalComposition = await rl.question("Enter chemical composition: ");
-  const diagnosticProperties = await rl.question("Enter diagnostic properties: ");
-  const name = await rl.question("Enter name: ");
 
-  const mineral = {
-    sampleNumber: sampleNumber,
-    color: color,
-    form: form,
-    lusture: lusture,
-    streak: streak,
-    hardness: hardness,
-    cleavage: cleavage,
-    fracture: fracture,
-    specificGravity: specificGravity,
-    chemicalComposition: chemicalComposition,
-    diagnosticProperties: diagnosticProperties,
-    name: name
+  const mineral = {};
+
+  const properties = ["sample number", "color", "form", "lusture", "streak", "hardness", "cleavage", "fracture", "specific gravity", "chemical composition", "diagnostic properties", "name"];
+
+  for (let property of properties) {
+    const value = await rl.question(`Enter ${property}: `);
+    property = property.replace(" ", "").toUpperCase();
+    mineral[property] = value;
   }
 
   console.log(mineral);
 
+  fs.writeFileSync(`./minerals/${mineral.NAME}.json`, JSON.stringify(mineral))
+
   process.exit();
+
 }
 
-main()
+main();
